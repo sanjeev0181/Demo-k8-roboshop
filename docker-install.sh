51a368876028a15800fbdc109f0b4071c5072782
sanjeev0181 <Adityasanjeev1996@gmail.com>
2023-04-03 16:57:37 +0530

    adding the awscli

--- docker-install.sh
+++ docker-install.sh
@@ -1,2 +1,10 @@
+sudo apt update
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
+
+sudo usermod -aG docker $USER
+sudo chmod 666 /var/run/docker.sock
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
