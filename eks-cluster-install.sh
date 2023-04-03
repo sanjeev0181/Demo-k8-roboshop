3dcb6094d49695271c35aece2441bdcbe619af20
sanjeev0181 <Adityasanjeev1996@gmail.com>
2023-04-03 18:14:13 +0530

    small change in files

--- eks-cluster-install.sh
+++ eks-cluster-install.sh
@@ -8,3 +8,5 @@ curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/d
 sudo mv /tmp/eksctl /usr/local/bin
 eksctl version
 
+#https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html
+#aws cli update ccommand
