745b85356d93121b8cfae44d82baa366b43ee82d
sanjeev0181 <Adityasanjeev1996@gmail.com>
2023-04-03 17:14:48 +0530

    adding the els-custer install step

---
+++ eks-cluster-install.sh
@@ -0,0 +1,4 @@
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.24.10/2023-01-30/bin/linux/amd64/kubectl
+sudo mv kubectl /usr/local/bin/kubectl
+chmod +x /usr/local/bin/kubectl
+kubectl version
