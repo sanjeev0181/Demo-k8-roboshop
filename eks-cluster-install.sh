eed226015a44214957227df11bf3bebf7618656d
sanjeev0181 <Adityasanjeev1996@gmail.com>
2023-04-03 17:19:45 +0530

    adding the els-custers install step

--- eks-cluster-install.sh
+++ eks-cluster-install.sh
@@ -2,3 +2,9 @@ curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.24.10/2023-01-30/bin/lin
 sudo mv kubectl /usr/local/bin/kubectl
 chmod +x /usr/local/bin/kubectl
 kubectl version
+
+#---
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+sudo mv /tmp/eksctl /usr/local/bin
+eksctl version
+
