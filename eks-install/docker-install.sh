493ddecd2c58cfe0dab6461c4da02883e8670a69
sanjeev0181 <Adityasanjeev1996@gmail.com>
2023-04-04 17:58:37 +0530

    remove some ex line in docker install

--- eks-install/docker-install.sh
+++ eks-install/docker-install.sh
@@ -5,10 +5,4 @@ sh get-docker.sh
 sudo usermod -aG docker $USER
 sudo chmod 666 /var/run/docker.sock
 
-#install commands
-apt  install awscli -y
-#update version
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-apt install unzip -y
-unzip awscliv2.zip
-sudo ./aws/install
+#chmod +x <filename>.sh
